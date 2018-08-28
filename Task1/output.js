54ccf3a83b6b61dd9260136a9981c46ec42958bf
Ankitkumar Boghra <32591079+ankitboghra@users.noreply.github.com>
2018-08-28 07:13:36 +0530

    Task 2
    Psudo implementation

--- Task1/output.js
+++ Task1/output.js
@@ -4,25 +4,46 @@ collectionName = "test";
 bucketSize = 2;
 
 const submission = async (client, dbName, collectionName, bucketSize) => {
-  var client = client;
-  var dbName = dbName;
-  var bucketSize = bucketSize
+  // var client = client;
+  // var dbName = dbName;
+  // var bucketSize = bucketSize
 
   let promise = new Promise((resolve, reject) => {
-    //Code for data fetch and aggregation
-
-    //Code end
-    setTimeout(console.log("code"), 1000);
-    // resolve("done!")
+    //Code for connection
+    //Connection
+    const connection = await MongoClient.connect(client, { useNewUrlParser: true });
+    const db = connection.db(dbName);
+    //Code ends for connection
   })
+  //Code for data fetch
+  let promise1 = new Promise((resolve, reject) => {
+    db.collection(collectionName, function (err, collection) {
+      collection
+        .find()
+        .toArray(function (err, data) {
+          let cary = {};
+          data.map(curData => {
+            cary[curData.category] = [...(cary[curData.category] || []),
+            curData._id];
+          });
+        })
+    })
+  })
+  //Code ends for data fetch
 
-  submission(client, dbName, collectionName, bucketSize).then(resolve("done"));
-
-  let ans = await submission;
-
-  console.log("loadash");
-  //code for bucket from loadash
-
+  //Code for data aggregation using loadash.js
+  let promise2 = new Promise((resolve, reject) => {
+    Object.keys(cary).map(ans => {
+      let tempAns = {
+        category: ans,
+        id: cary[ans]
+      };
+      console.log(tempAns.category);
+      console.log(_lodash.chunk(tempAns.id, bucketSize));
+    });
+  });
+  //Code ends for data aggregation using loadash.js
 }
-
 submission();
+
+connection.close();
